c6c46c3b9a7758b82618bb61c3f83c56d78adab4
CadenIrvine <irvine-c20@ulster.ac.uk>
2026-09-22 10:43:06 +0100

    Week1Calculator

--- Week1CalculatorApp/Program.cs
+++ Week1CalculatorApp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in the first number, followed by enter:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number, followed by enter:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives {2}", firstNumber, secondNumber, result);
